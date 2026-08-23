a68bff374cccdcecd7037167e6351be6f7afc4b1
Alejandro J. Gómez <gmz248alejandro@gmail.com>
2026-08-23 15:25:27 -0300

    style: añadir icono al proyecto

--- ClaammApp.UI/Ventanas.cs
+++ ClaammApp.UI/Ventanas.cs
@@ -1,11 +1,17 @@
 using System.Windows;
+using System.Windows.Media;
 
 namespace ClaammApp.UI;
 
 public static class Ventanas
 {
+    private static readonly ImageSource IconoAplicacion =
+        new System.Windows.Media.Imaging.BitmapImage(new Uri("pack://application:,,,/favico.ico"));
+
     public static void AjustarAlAreaTrabajo(Window ventana)
     {
+        ventana.Icon = IconoAplicacion;
+
         var area = SystemParameters.WorkArea;
 
         ventana.Width = Math.Min(ventana.Width, area.Width * 0.95);
